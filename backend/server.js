c0be3c9a13c36b63d640795b1488c8183a242ed1
tua <soumavabiswas15@gmail.com>
2023-08-24 23:20:45 +0530

    connected to mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const Task = require('./models/task')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
